64f4a6216c21bb12df297b5365ceee7b38390bb0
mohmmad <mhmd.salomi.123@gmail.comgit config --global user.name mohmmadgit config --global user.email mhmd.salomi.123@gmail.com>
2023-09-25 12:02:12 +0300

    step 7

--- index.js
+++ index.js
@@ -7,19 +7,15 @@ app.get("/",(req,res)=> {
 app.get('/test',(req,res)=>{
     res.json({status:200,message:"ok it's done"})
 })
-
 app.get('/time',(req,res)=>{
     res.json({status:200, message:Date()})
 })
-
 app.listen(3000,()=>{
     console.log("connected on port 3000")
 })
-
 app.get('/hello/<ID>',(req,res)=>{
     res.json({status:200, message:"Hello, <ID>"()})
 })
-
 app.get('/search?s=<SEARCH>', (req, res) => {
     const { s } = req.query;
     if (s) {
@@ -34,24 +30,34 @@ const movies = [
     { title: 'Brazil', year: 1985, rating: 8 },
     { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ];
-
 app.get('/movies/read', (req, res) => {
     res.json({ status: 200, data: movies });
 });
-
 app.get('/movies/read/by-date', (req, res) => {
     const moviesByDate = movies.sort((a, b) => a.year - b.year);
     res.json({ status: 200, data: moviesByDate });
 });
-
-
 app.get('/movies/read/by-rating', (req, res) => {
     const moviesByRating = movies.sort((a, b) => b.rating - a.rating);
     res.json({ status: 200, data: moviesByRating });
 });
-
-
 app.get('/movies/read/by-title', (req, res) => {
     const moviesByTitle = movies.sort((a, b) => a.title.localeCompare(b.title));
     res.json({ status: 200, data: moviesByTitle });
+});
+app.get('/movies/read/id/:id', (req, res) => {
+  const movieId = parseInt(req.params.id);
+  const movie = movies[movieId - 1]; 
+  if (!movie) {
+    res.status(404).json({
+      status: 404,
+      error: true,
+      message: `the movie ${movieId} does not exist`,
+    });
+  } else {
+    res.status(200).json({
+      status: 200,
+      data: movie,
+    });
+  }
 });
